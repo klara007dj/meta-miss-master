fa2efff3d02179bb4e0e4bfc5aa335a5597966b5
xhris2006 <diorrebero90@icloud.com>
2026-05-06 08:51:43 +0100

    Add rate limiter to ranking routes

--- backend/src/routes/ranking.routes.js
+++ backend/src/routes/ranking.routes.js
@@ -1,10 +1,11 @@
 // ranking.routes.js
 const express = require("express");
 const rankingController = require("../controllers/ranking.controller");
+const { rankingRateLimiter } = require("../middlewares/rateLimiter");
 const router = express.Router();
 
-router.get("/", rankingController.getGlobalRanking);
-router.get("/top", rankingController.getTopN);
+router.get("/", rankingRateLimiter, rankingController.getGlobalRanking);
+router.get("/top", rankingRateLimiter, rankingController.getTopN);
 router.get("/stats", rankingController.getStats);
 
 module.exports = router;
