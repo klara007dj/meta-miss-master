2f9b11bd6ba3b5be55b737904a16217de182f96b
klara007dj <Klaradjeumen@gmail.com>
2026-06-25 18:28:15 +0100

    Add resetVotes route to admin routes

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -19,6 +19,7 @@ router.get("/users", adminController.getAllUsers);
 router.delete("/users/:id", adminController.deleteUser);
 router.patch("/users/:id", adminController.updateUser);
 router.delete("/votes/:id", adminController.deleteVote);
+router.post("/votes/reset", adminController.resetVotes);
 router.get("/contests", adminController.getContests);
 router.post("/contest", adminController.createContest);
 router.patch("/contest/:id", adminController.updateContest);
